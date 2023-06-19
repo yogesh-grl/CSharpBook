23cb2e1093a32d2ea6081272113050931c50ed0f
yogesh-grl <yogesh@graniteriverlabs.in>
2023-06-19 17:49:38 +0530

    SOLID Design Principles

---
+++ CSharpBook/SOLID/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBook.SOLID
+{
+    /// <summary>
+    /// Class reponsible for the sending the Email 
+    /// </summary>
+    class Email
+    {
+        public Email(Employee empyData) { }
+
+        public void SendEmail()
+        {
+            //Send the Employee data via mail to the respective target 
+        }
+    }
+
+    /// <summary>
+    /// Class responsible for adding new employee
+    /// </summary>
+    class EmployeeData
+    {
+        public EmployeeData(Employee empyData) { }
+
+        public void AddEmployeeData()
+        {
+            //add employee 
+        }
+    }
+
+    /// <summary>
+    /// Main Employee model
+    /// </summary>
+    class Employee
+    {
+        public Employee()
+        {
+        }
+    }
+
+
+    internal class SingleResponsibilityPrinciple
+    {
+        public SingleResponsibilityPrinciple()
+        {
+        }
+
+        public void MainMethod()
+        {
+
+        }
+    }
+}
