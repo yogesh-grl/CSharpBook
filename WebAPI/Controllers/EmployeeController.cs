caaca4ba55123809e6cb6d05d961e3f18e1b1ffe
yogesh-grl <yogesh@graniteriverlabs.in>
2023-06-21 11:23:01 +0530

    web api sample

---
+++ WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    class Employee
+    {
+        string EmployeeName;
+        int EmployeeID;
+        public Employee(string name, int id)
+        {
+            EmployeeName = name;
+            EmployeeID = id;
+        }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        [HttpGet("Employee")]
+        public IActionResult GetEmployee()
+        {
+            return Ok(new Employee("Yogesh", 1));
+        }
+
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
