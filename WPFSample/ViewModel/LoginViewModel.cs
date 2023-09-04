4ec25fe2aa77714020fa4b26db74ce3407a52774
yogesh-grl <yogesh@graniteriverlabs.in>
2023-09-04 10:44:59 +0530

    WPF sample
    WPF sample project

---
+++ WPFSample/ViewModel/LoginViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFSample.Model;
+
+namespace WPFSample.ViewModel
+{
+    internal class LoginViewModel : BaseViewModel
+    {
+        LoginModel objLoginModel = null;
+
+        public string _Login { get; set; }
+        public string _Password { get; set; }
+
+        public LoginViewModel()
+        {
+            objLoginModel = new LoginModel();
+        }
+
+        public string Login
+        {
+            get
+            {
+                return _Login;
+            }
+            set
+            {
+                _Login = value;
+                NotifyPropertyChanged(nameof(Login));
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                _Password = value;
+                NotifyPropertyChanged(nameof(Password));
+            }
+        }
+
+    }
+}
