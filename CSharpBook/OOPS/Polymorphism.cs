27c2351fdc3b60c121ee7451fd102e118702477c
Hema <yogesh@graniteriverlabs.in>
2024-03-12 10:47:27 +0530

    Create Polymorphism.cs

---
+++ CSharpBook/OOPS/Polymorphism.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBook.OOPS
+{
+    /// <summary>
+    /// Polymorphism - Many form 
+    /// It will treat the obj of diff classes to a common super class
+    /// </summary>
+    internal class Polymorphism
+    {
+        //Obj for TestCaase1 
+        TestCase testCase1 = new TestCase1();
+        //Obj for TestCaase
+        TestCase testCase2 = new TestCase2();
+
+        public void MainMethod()
+        {
+            testCase1.Configuration();
+            testCase1.Validation();
+
+            testCase2.Configuration();
+            testCase2.Validation();
+        }
+    }
+
+    /// <summary>
+    /// Primary Class
+    /// </summary>
+    public abstract class TestCase
+    {
+        public abstract void Configuration();
+        public abstract void Validation();
+
+    }
+
+    public class TestCase1 : TestCase
+    {
+        public TestCase1() { }
+
+        public override void Configuration()
+        {
+            Console.WriteLine("$Class 1 - Config");
+        }
+
+        public override void Validation()
+        {
+            Console.WriteLine("$Class 1 - Validation");
+        }
+    }
+
+    public class TestCase2 : TestCase
+    {
+        public TestCase2() { }
+
+        public override void Configuration()
+        {
+            Console.WriteLine("$Class 1 - Config");
+        }
+
+        public override void Validation()
+        {
+            Console.WriteLine("$Class 1 - Validation");
+        }
+    }
+}
