048ab3c1e2a4bbf7687d1d91e6cde02a82ae80c2
Yogesh <yogesh@graniteriverlabs.in>
2024-12-26 17:02:58 +0530

    Updated Web App Project

---
+++ WebAPI Architecture using C#/Helper/CustomAttribute.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppSample.Helper
+{
+    public class CustomAttribute
+    {
+    }
+
+    public class ValidateEmailDomainAttribute : ValidationAttribute
+    {
+        private readonly string _allowedDomain;
+        public ValidateEmailDomainAttribute(string allowedDomain)
+        {
+            _allowedDomain = allowedDomain;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            string[] strs = value.ToString().Split('@');
+            return strs[0].ToUpper() == _allowedDomain.ToUpper();
+        }
+    }
+}
---
+++ WebAPI Architecture using C#/Helper/Middleware/TaskMiddleware/BasicAuthHandler.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace WebAppSample.Helper.Middleware.TaskMiddleware
+{
+    public class BasicAuthHandler
+    {
+        private readonly RequestDelegate _next;
+        public BasicAuthHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            if (!httpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                httpContext.Response.StatusCode = 401;
+                await httpContext.Response.WriteAsync("Unauthorized");
+                return;
+            }
+            else
+            {
+                var header = httpContext.Request.Headers["Authorization"].ToString();
+                var encodedCreds = header.Substring(6);
+                var creds = Encoding.UTF8.GetString(Convert.FromBase64String(encodedCreds));
+                string[] uiPwd = creds.Split(":");
+                var uid = uiPwd[0];
+                var password = uiPwd[1];
+
+                if (Authenticate(uid, password))
+                {
+                    httpContext.Response.StatusCode = 401;
+                    await httpContext.Response.WriteAsync("Unauthorized");
+                    return;
+                }
+                else
+                {
+                    //await _next(httpContext);
+                }
+            }
+            await _next(httpContext);
+        }
+
+        private bool Authenticate(string UserName, string Pwd)
+        {
+            if (UserName != "Yogesh" || Pwd != "Password")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
